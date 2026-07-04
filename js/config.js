084165266aa31290015cee238bc938e1f03382a7
perkabem-mpw <perkabem@gmail.com>
2026-07-04 09:29:23 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,7 @@
+const CONFIG = {
 
+    API_URL: "https://script.google.com/macros/s/AKfycby-fqecjb73al_AT8vmQoG9LWqPeEtyN_-WyjrJwkM2ph4Fdk95xK2NmhqqEM5GSfcehw/exec",
+
+    EVENT_ID: "EVT0001"
+
+};
