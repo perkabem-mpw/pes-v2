cbcf0a922f29306bf56423dddc348f5ffd7d97e0
perkabem-mpw <perkabem@gmail.com>
2026-07-04 09:30:20 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,27 @@
+async function api(module, action, data = {}) {
 
+    const response = await fetch(CONFIG.API_URL, {
+
+        method: "POST",
+
+        headers: {
+
+            "Content-Type":"application/json"
+
+        },
+
+        body: JSON.stringify({
+
+            module,
+
+            action,
+
+            ...data
+
+        })
+
+    });
+
+    return await response.json();
+
+}
